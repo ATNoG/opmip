97bfab0cc2fabade17c4f799bc6205b365d9ea83
Bruno Santos <bsantos@av.it.pt>
2010-03-17 17:54:07 +0000

    Add templates for header, source and Jamfile
    This avoids the inconvenience of having to copy from the top and bottom of
    another file when creating a new one.

---
+++ templates/header.hpp
@@ -0,0 +1,34 @@
+//=============================================================================
+// Brief   : <Brief Description>
+// Authors : Bruno Santos <bsantos@av.it.pt>
+// ----------------------------------------------------------------------------
+// OPMIP - Open Proxy Mobile IP
+//
+// Copyright (C) 2010 Universidade de Aveiro
+// Copyrigth (C) 2010 Instituto de Telecomunicações - Pólo de Aveiro
+//
+// This software is distributed under a license. The full license
+// agreement can be found in the file LICENSE in this distribution.
+// This software may not be copied, modified, sold or distributed
+// other than expressed in the named license agreement.
+//
+// This software is distributed without any warranty.
+//=============================================================================
+
+#ifndef OPMIP_<PATH_TO>__HPP_
+#define OPMIP_<PATH_TO>__HPP_
+
+///////////////////////////////////////////////////////////////////////////////
+#include <opmip/base.hpp>
+
+///////////////////////////////////////////////////////////////////////////////
+namespace opmip {
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+} /* namespace opmip */
+
+// EOF ////////////////////////////////////////////////////////////////////////
+#endif /* OPMIP_<PATH_TO>__HPP_ */
