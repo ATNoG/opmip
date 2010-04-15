4801723ffdaeff80c7d7f55ddb10eb5b1ac135e5
Bruno Santos <bsantos@av.it.pt>
2010-04-15 11:21:12 +0100

    [ip/ll]: Introduce some link layer definitions
    Introduce the technology and mac_address types that were initial defined for
    the bcache.

---
+++ inc/opmip/ll/mac_address.hpp
@@ -0,0 +1,37 @@
+//=============================================================================
+// Brief   : Link Layer MAC Address
+// Authors : Bruno Santos <bsantos@av.it.pt>
+// ----------------------------------------------------------------------------
+// OPMIP - Open Proxy Mobile IP
+//
+// Copyright (C) 2010 Universidade de Aveiro
+// Copyrigth (C) 2010 Instituto de Telecomunicações - Pólo de Aveiro
+//
+// This software is distributed under a license. The full license
+// agreement can be found in the file LICENSE in this distribution.
+// This software may not be copied, modified, sold or distributed
+// other than expressed in the named license agreement.
+//
+// This software is distributed without any warranty.
+//=============================================================================
+
+#ifndef OPMIP_LL_MAC_ADDRESS__HPP_
+#define OPMIP_LL_MAC_ADDRESS__HPP_
+
+///////////////////////////////////////////////////////////////////////////////
+#include <opmip/base.hpp>
+
+///////////////////////////////////////////////////////////////////////////////
+namespace opmip { namespace ll {
+
+///////////////////////////////////////////////////////////////////////////////
+struct mac_address {
+	uint8 address[16];
+	uint8 length;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+} /* namespace ll */ } /* namespace opmip */
+
+///////////////////////////////////////////////////////////////////////////////
+#endif /* OPMIP_LL_MAC_ADDRESS__HPP_ */
