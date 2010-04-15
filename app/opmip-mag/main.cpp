7cd1fd2866966277e42374000b44656b6df7a064
Bruno Santos <bsantos@av.it.pt>
2010-04-15 17:21:47 +0100

    Create skeletons for the LMA and MAG applications

---
+++ app/opmip-mag/main.cpp
@@ -0,0 +1,47 @@
+//=============================================================================
+// Brief   : Mobile Access Gateway Main Entry Point
+// Authors : Bruno Santos <bsantos@av.it.pt>
+// ----------------------------------------------------------------------------
+// OPMIP - Open Proxy Mobile IP
+//
+// Copyright (C) 2010 Universidade de Aveiro
+// Copyrigth (C) 2010 Instituto de Telecomunicações - Pólo de Aveiro
+//
+// This software is distributed under a license. The full license
+// agreement can be found in the file LICENSE in this distribution.
+// This software may not be copied, modified, sold or distributed
+// other than expressed in the named license agreement.
+//
+// This software is distributed without any warranty.
+//=============================================================================
+
+#include <opmip/base.hpp>
+#include <boost/bind.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/asio/io_service.hpp>
+#include <iostream>
+#include "mag.hpp"
+
+///////////////////////////////////////////////////////////////////////////////
+int main()
+{
+	size_t concurrency = boost::thread::hardware_concurrency();
+	boost::asio::io_service ios(concurrency);
+	boost::thread_group tg;
+
+	for (size_t i = 1; i < concurrency; ++i)
+		tg.create_thread(boost::bind(&boost::asio::io_service::run, &ios));
+
+	try {
+		ios.run();
+		tg.join_all();
+
+	} catch(std::exception& e) {
+		std::cerr << "error: " << e.what() << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
+
+// EOF ////////////////////////////////////////////////////////////////////////
