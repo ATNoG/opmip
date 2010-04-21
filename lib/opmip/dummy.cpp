ad8160128c375a241bea7e7c57b41705b63a8b2a
Bruno Santos <bsantos@av.it.pt>
2010-04-21 17:37:18 +0100

    [ip]: Introduce the IPv6 ICMP data structures

--- lib/opmip/dummy.cpp
+++ lib/opmip/dummy.cpp
@@ -1,6 +1,7 @@
 #include <opmip/bcache.hpp>
 #include <opmip/bulist.hpp>
 #include <opmip/ip/mproto.hpp>
+#include <opmip/ip/icmp.hpp>
 #include <opmip/sys/netlink/frame.hpp>
 #include <opmip/sys/rtnetlink/attribute.hpp>
 #include <opmip/sys/rtnetlink/message.hpp>
