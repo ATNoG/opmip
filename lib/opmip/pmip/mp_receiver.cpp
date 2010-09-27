3caed9eff55cff97660890179c419289bf10f7ed
Bruno Santos <bsantos@av.it.pt>
2010-09-27 16:56:31 +0100

    [pmip/mp_receiver]: Teach mproto options parser about network prefix option

--- lib/opmip/pmip/mp_receiver.cpp
+++ lib/opmip/pmip/mp_receiver.cpp
@@ -21,9 +21,10 @@
 namespace opmip { namespace pmip {
 
 ///////////////////////////////////////////////////////////////////////////////
-static bool parse_pbu_options(uchar* data, size_t length, proxy_binding_info& pbinfo)
+static bool parse_options(uchar* data, size_t length, proxy_binding_info& pbinfo)
 {
 	ip::mproto::option*            opt;
+	ip::mproto::option::netprefix* npf;
 	ip::mproto::option::nai*       nai = nullptr;
 	ip::mproto::option::handoff*   hof = nullptr;
 	ip::mproto::option::att*       att = nullptr;
@@ -43,6 +44,11 @@ static bool parse_pbu_options(uchar* data, size_t length, proxy_binding_info& pb
 				return false;
 
 			pbinfo.id.assign(nai->id, opt->length - 1);
+			break;
+
+		case ip::mproto::option::netprefix::type_value:
+			npf = opt->get<ip::mproto::option::netprefix>();
+			pbinfo.prefix_list.push_back(ip::prefix_v6(npf->prefix, npf->length));
 			break;
 
 		case ip::mproto::option::handoff::type_value:
@@ -85,7 +91,7 @@ bool pbu_receiver::parse(size_t rbytes, proxy_binding_info& pbinfo)
 	pbinfo.sequence = pbu->sequence();
 	pbinfo.lifetime = 4000 * pbu->lifetime();
 
-	return parse_pbu_options(_buffer + pos, rbytes - pos, pbinfo);
+	return parse_options(_buffer + pos, rbytes - pos, pbinfo);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -107,7 +113,7 @@ bool pba_receiver::parse(size_t rbytes, proxy_binding_info& pbinfo)
 	pbinfo.lifetime = 4000 * pba->lifetime();
 	pbinfo.status   = pba->status();
 
-	return parse_pbu_options(_buffer + pos, rbytes - pos, pbinfo);
+	return parse_options(_buffer + pos, rbytes - pos, pbinfo);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
