60d6375a5d658895a2791343f6477578d6b42fb2
Bruno Santos <bsantos@av.it.pt>
2011-05-28 15:47:24 +0100

    app: refactor madwifi driver

--- app/opmip-mag/driver.cpp
+++ app/opmip-mag/driver.cpp
@@ -17,73 +17,16 @@
 
 #include "driver.hpp"
 #include "drivers/madwifi_driver.hpp"
-#include <opmip/logger.hpp>
-#include <boost/bind.hpp>
 #include <boost/make_shared.hpp>
-#include <iostream>
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 namespace opmip { namespace app {
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-opmip::logger log_("madwifi", std::cout);
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-class madwifi_drv : public driver {
-	static void link_event(const boost::system::error_code& ec, const madwifi_driver::event& ev,
-	                       pmip::mag& mag)
-	{
-		if (ec)
-			return;
-
-		opmip::pmip::mag::attach_info ai(ev.if_index,
-										 ev.if_address,
-										 ev.mn_address);
-
-		switch (ev.which) {
-		case opmip::app::madwifi_driver_impl::attach:
-			mag.mobile_node_attach(ai, [ev](uint ec) {
-				log_(0, "node ", ev.mn_address, " attachment completed with code ", ec);
-			});
-			break;
-
-		case opmip::app::madwifi_driver_impl::detach:
-			mag.mobile_node_detach(ai, [ev](uint ec) {
-				log_(0, "node ", ev.mn_address, " detachment completed with code ", ec);
-			});
-			break;
-
-		default:
-			break;
-		}
-	}
-
-public:
-	madwifi_drv(boost::asio::io_service& ios)
-		: _drv(ios)
-	{ }
-
-	virtual void start(pmip::mag& mag, const boost::any& parameter)
-	{
-		auto interfaces = boost::any_cast<const std::vector<std::string>&>(parameter);
-
-		_drv.start(interfaces, boost::bind(&madwifi_drv::link_event, _1, _2, boost::ref(mag)));
-	}
-
-	virtual void stop()
-	{
-		_drv.stop();
-	}
-
-private:
-	opmip::app::madwifi_driver _drv;
-};
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 driver_ptr make_driver(boost::asio::io_service& ios, const std::string& name)
 {
 	if (name == "madwifi")
-		return boost::make_shared<madwifi_drv>(ios);
+		return boost::make_shared<madwifi_driver>(ios);
 
 	return driver_ptr();
 }
