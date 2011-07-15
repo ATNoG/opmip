bbd5f877ff92cbd365051403d098b67cdf9043d4
Bruno Santos <bsantos@av.it.pt>
2011-07-15 10:51:50 +0100

    Make the logger compatible with compilers that do not support C++11

--- inc/opmip/logger.hpp
+++ inc/opmip/logger.hpp
@@ -1,7 +1,7 @@
-//=============================================================================
+//===========================================================================================================
 // Brief   : Logging Facilities
 // Authors : Bruno Santos <bsantos@av.it.pt>
-// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------
 // OPMIP - Open Proxy Mobile IP
 //
 // Copyright (C) 2010 Universidade de Aveiro
@@ -13,21 +13,22 @@
 // other than expressed in the named license agreement.
 //
 // This software is distributed without any warranty.
-//=============================================================================
+//===========================================================================================================
 
 #ifndef OPMIP_LOGGER__HPP_
 #define OPMIP_LOGGER__HPP_
 
-///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <opmip/base.hpp>
 #include <boost/utility.hpp>
 #include <ostream>
 
-///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 namespace opmip {
 
-///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class logger : boost::noncopyable {
+#ifndef BOOST_NO_VARIADIC_TEMPLATES
 	template<class T, class ...Args>
 	static void print_args(std::ostream& os, const T& arg, const Args& ...args)
 	{
@@ -40,6 +41,7 @@ class logger : boost::noncopyable {
 	{
 		os << arg << std::endl;
 	}
+#endif
 
 public:
 	logger(char const* const name, std::ostream& sink)
@@ -51,6 +53,7 @@ public:
 	uint          level() const { return _level; }
 	std::ostream& sink() const  { return _sink; }
 
+#ifndef BOOST_NO_VARIADIC_TEMPLATES
 	template<class ...T>
 	void operator()(uint level, const T& ...args)
 	{
@@ -61,6 +64,117 @@ public:
 		print_args(_sink, args...);
 		std::flush(_sink);
 	}
+#else
+	template<class T1>
+	void operator()(uint level, const T1& arg1)
+	{
+		if ((level > _level) || !_sink)
+			return;
+
+		_sink << _name << ": " << arg1 << std::endl;
+		std::flush(_sink);
+	}
+
+	template<class T1, class T2>
+	void operator()(uint level, const T1& arg1, const T2& arg2)
+	{
+		if ((level > _level) || !_sink)
+			return;
+
+		_sink << _name << ": " << arg1 << arg2 << std::endl;
+		std::flush(_sink);
+	}
+
+	template<class T1, class T2, class T3>
+	void operator()(uint level, const T1& arg1, const T2& arg2, const T3& arg3)
+	{
+		if ((level > _level) || !_sink)
+			return;
+
+		_sink << _name << ": " << arg1 << arg2 << arg3 << std::endl;
+		std::flush(_sink);
+	}
+
+	template<class T1, class T2, class T3, class T4>
+	void operator()(uint level, const T1& arg1, const T2& arg2, const T3& arg3, const T4& arg4)
+	{
+		if ((level > _level) || !_sink)
+			return;
+
+		_sink << _name << ": " << arg1 << arg2 << arg3 << arg4 << std::endl;
+		std::flush(_sink);
+	}
+
+	template<class T1, class T2, class T3, class T4, class T5>
+	void operator()(uint level, const T1& arg1, const T2& arg2, const T3& arg3, const T4& arg4, const T5& arg5)
+	{
+		if ((level > _level) || !_sink)
+			return;
+
+		_sink << _name << ": " << arg1 << arg2 << arg3 << arg4 << arg5 << std::endl;
+		std::flush(_sink);
+	}
+
+	template<class T1, class T2, class T3, class T4, class T5, class T6>
+	void operator()(uint level, const T1& arg1, const T2& arg2, const T3& arg3, const T4& arg4, const T5& arg5, const T6& arg6)
+	{
+		if ((level > _level) || !_sink)
+			return;
+
+		_sink << _name << ": " << arg1 << arg2 << arg3 << arg4 << arg5 << arg6 << std::endl;
+		std::flush(_sink);
+	}
+
+	template<class T1, class T2, class T3, class T4, class T5, class T6, class T7>
+	void operator()(uint level, const T1& arg1, const T2& arg2, const T3& arg3, const T4& arg4, const T5& arg5, const T6& arg6, const T7& arg7)
+	{
+		if ((level > _level) || !_sink)
+			return;
+
+		_sink << _name << ": " << arg1 << arg2 << arg3 << arg4 << arg5 << arg6 << arg7 << std::endl;
+		std::flush(_sink);
+	}
+
+	template<class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8>
+	void operator()(uint level, const T1& arg1, const T2& arg2, const T3& arg3, const T4& arg4, const T5& arg5, const T6& arg6, const T7& arg7, const T8& arg8)
+	{
+		if ((level > _level) || !_sink)
+			return;
+
+		_sink << _name << ": " << arg1 << arg2 << arg3 << arg4 << arg5 << arg6 << arg7 << arg8 << std::endl;
+		std::flush(_sink);
+	}
+
+	template<class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9>
+	void operator()(uint level, const T1& arg1, const T2& arg2, const T3& arg3, const T4& arg4, const T5& arg5, const T6& arg6, const T7& arg7, const T8& arg8, const T9& arg9)
+	{
+		if ((level > _level) || !_sink)
+			return;
+
+		_sink << _name << ": " << arg1 << arg2 << arg3 << arg4 << arg5 << arg6 << arg7 << arg8 << arg9 << std::endl;
+		std::flush(_sink);
+	}
+
+	template<class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, class T10>
+	void operator()(uint level, const T1& arg1, const T2& arg2, const T3& arg3, const T4& arg4, const T5& arg5, const T6& arg6, const T7& arg7, const T8& arg8, const T9& arg9, const T10& arg10)
+	{
+		if ((level > _level) || !_sink)
+			return;
+
+		_sink << _name << ": " << arg1 << arg2 << arg3 << arg4 << arg5 << arg6 << arg7 << arg8 << arg9 << arg10 << std::endl;
+		std::flush(_sink);
+	}
+
+	template<class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, class T10, class T11>
+	void operator()(uint level, const T1& arg1, const T2& arg2, const T3& arg3, const T4& arg4, const T5& arg5, const T6& arg6, const T7& arg7, const T8& arg8, const T9& arg9, const T10& arg10, const T11& arg11)
+	{
+		if (level > _level)
+			return;
+
+		_sink << _name << ": " << arg1 << arg2 << arg3 << arg4 << arg5 << arg6 << arg7 << arg8 << arg9 << arg10 << arg11 << std::endl;
+		std::flush(_sink);
+	}
+#endif
 
 private:
 	char const* const _name;
@@ -68,8 +182,8 @@ private:
 	uint              _level;
 };
 
-///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 } /* namespace opmip */
 
-// EOF ////////////////////////////////////////////////////////////////////////
+// EOF //////////////////////////////////////////////////////////////////////////////////////////////////////
 #endif /* OPMIP_LOGGER__HPP_ */
